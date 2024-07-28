7da7f31c42142d8024c9fa28cf0749fbf34b286b
Parth Verma <parth50verma@gmail.com>
2024-07-28 20:46:14 +0530

    title update

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def createTables():
     conn.close()
     
 def main():
+    st.set_page_config(page_title = "TradeMate")
     createTables()
     with open("style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
